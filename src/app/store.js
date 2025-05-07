3c2e4e28bed5e9f7daef78977625317a1e68b54f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-07 19:04:41 +0530

    Todo component  Provider and useSelector

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,5 @@ import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from "../features/todo/todoSlice.jsx"
 
 export const store = configureStore({
-    reducers: todoReducer,
-})
+    reducer: todoReducer,
+});
