33108942286928f4e551a216f1ab81a1112683c7
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-07 18:09:47 +0530

    creating reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from "../features/todo/todoSlice.jsx"
 
 export const store = configureStore({
-    reducers: {
-
-    },
+    reducers: todoReducer,
 })
